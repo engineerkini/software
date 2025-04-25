15b384353367e4c5c5a06b23686031736763784d
fidele <fidelniyomugabo67@gmail.com>
2025-04-25 16:24:58 +0200

    add:google analysis

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "./components/layout/Footer";
 import LoadingBar from "./components/LoadingBar";
 import { Suspense } from "react";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+ <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1F98T9CZTH"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1F98T9CZTH');
+          `}
+        </Script>
+      </head>
+
       <body
         className={` bg-white ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
